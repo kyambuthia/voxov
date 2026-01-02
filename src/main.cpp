98407a63683f7ea5cf9f465ed3ea3b728a640e22
kyambuthia <kyambuthia@gmail.com>
2026-01-02 20:16:29 +0300

    we've got a window, bois

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,52 @@
+#define SDL_MAIN_USE_CALLBACKS
+
+#include <iostream>
 #include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
 
-#include "vulkan/vulkan.h"
+static SDL_Window *window = nullptr;
+static SDL_Renderer *renderer = nullptr;
 
-static SDL_Window* window = NULL;
-
-int main(int argc, char* argv[])
-{
-    if (!SDL_Init(SDL_INIT_VIDEO))
-    {
-        SDL_Log("ERR: Video init failed. %s", SDL_GetError());
-        return 1;
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        SDL_Log("init failed: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    window = SDL_CreateWindow("voxov", 640, 480, SDL_WINDOW_VULKAN);
-    if (!window)
-    {
-        SDL_Log("ERR: Window creation failed. %s", SDL_GetError());
-        return 1;
-    }
+    window = SDL_CreateWindow(
+        "voxov",
+	800, 600,
+	SDL_WINDOW_RESIZABLE
+    );
 
-    bool running = true;
-    SDL_Event e;
-    while (running)
-    {
-        while(SDL_PollEvent(&e))
-        { 
-            if (e.type == SDL_EVENT_QUIT) 
-            {
-                running = false;
-            }
-        }
-    }
+    if (!window) return SDL_APP_FAILURE;
 
-    SDL_DestroyWindow(window);
+    renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) return SDL_APP_FAILURE;
+
+    std::cout << "SDL3 initialized successfully" << std::endl;
+    *appstate = nullptr;
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(16);
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     SDL_Quit();
-
-    return 0;
+    std::cout << "SDL3 quit" << std::endl;
 }
 

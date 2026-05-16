ccbd8a5bd9ea453ab0f3817ee5bcfed05baa454b
kyambuthia <kyambuthia@gmail.com>
2026-05-16 17:28:25 +0300

    world: add VoxelMaterial enum and material-aware chunk API
    Introduce VoxelMaterial enum (Air, Dirt, Grass, Stone) to replace
    bare uint8_t solid/air booleans. Add material-aware accessors:
    material(), set_material(), and refresh_surface_materials().
    Rename build_naive_mesh to build_greedy_mesh with voxel_scale param
    in preparation for greedy meshing.

--- src/engine_world/voxel_chunk.hpp
+++ src/engine_world/voxel_chunk.hpp
@@ -5,6 +5,13 @@
 
 #include "engine_render/render_types.hpp"
 
+enum class VoxelMaterial : uint8_t {
+  Air = 0,
+  Dirt = 1,
+  Grass = 2,
+  Stone = 3,
+};
+
 class VoxelChunk {
 public:
   static constexpr int CHUNK_X = 64;
@@ -16,10 +23,14 @@ public:
                                          int32_t chunk_z);
   void generate_spherical_planet_seeded(uint64_t world_seed);
   void generate_flat_ground(int ground_y);
+  VoxelMaterial material(int x, int y, int z) const;
   bool solid(int x, int y, int z) const;
+  void set_material(int x, int y, int z, VoxelMaterial material);
   void set_solid(int x, int y, int z, bool value);
+  void refresh_surface_materials();
 
-  RenderMesh build_naive_mesh(const glm::vec3 &origin = glm::vec3(0.0f)) const;
+  RenderMesh build_greedy_mesh(const glm::vec3 &origin = glm::vec3(0.0f),
+                               float voxel_scale = 1.0f) const;
   RenderMesh build_debug_grid(float span, float step) const;
   RenderMesh build_sky_placeholder(float size) const;
 
